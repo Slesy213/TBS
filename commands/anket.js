cb09ab68d6a51e70594e6d6a2d7b3193435f2880
Slesy213 <derdimvar2321@gmail.com>
2026-05-25 22:09:39 +0300

    Update anket.js

--- commands/anket.js
+++ commands/anket.js
@@ -1,1 +1,24 @@
+const { SlashCommandBuilder } = require('discord.js');
 
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('anket')
+        .setDescription('Anket oluşturur')
+        .addStringOption(option =>
+            option.setName('soru')
+                .setDescription('Anket sorusu')
+                .setRequired(true)),
+
+    async execute(interaction) {
+
+        const soru = interaction.options.getString('soru');
+
+        const mesaj = await interaction.reply({
+            content: `📊 **Anket:** ${soru}`,
+            fetchReply: true
+        });
+
+        await mesaj.react('👍');
+        await mesaj.react('👎');
+    }
+};
